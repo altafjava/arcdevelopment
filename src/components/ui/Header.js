f069cb3f598ba6e4c77a0bbe0a5cb25de1bbcd79
Altaf <samaltaf6@gmail.com>
2020-07-13 09:04:41 +0530

    /estimate route added

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -132,7 +132,7 @@ export default function Header(props) {
         setValue(newValue)
     }
     const menuOptions = [{ name: "Services", link: "/services", activeIndex: 1, selectedIndex: 0 }, { name: 'Custom Software Development', link: '/customsoftware', activeIndex: 1, selectedIndex: 1 }, { name: 'Mobile App Development', link: '/mobileapps', activeIndex: 1, selectedIndex: 2 }, { name: 'Website Development', link: '/websites', activeIndex: 1, selectedIndex: 3 }]
-    const routes = [{ name: "Home", link: "/", activeIndex: 0 }, { name: "Services", link: "/services", activeIndex: 1, ariaOwns: anchorEl ? 'simple-menu' : undefined, ariaHaspopup: anchorEl ? true : undefined, mouseOver: event => handleClick(event) }, { name: "The Revolution", link: "/revolution", activeIndex: 2 }, { name: "About Us", link: "/about", activeIndex: 3 }, { name: "Contact Us", link: "/contact", activeIndex: 4 }, { name: "Free Estimate", link: "/estimate", activeIndex: 5 }]
+    const routes = [{ name: "Home", link: "/", activeIndex: 0 }, { name: "Services", link: "/services", activeIndex: 1, ariaOwns: anchorEl ? 'simple-menu' : undefined, ariaHaspopup: anchorEl ? true : undefined, mouseOver: event => handleClick(event) }, { name: "The Revolution", link: "/revolution", activeIndex: 2 }, { name: "About Us", link: "/about", activeIndex: 3 }, { name: "Contact Us", link: "/contact", activeIndex: 4 }]
     // useEffect() means componentWillMount()
     useEffect(() => {
         [...menuOptions, ...routes].forEach(route => {
@@ -157,7 +157,7 @@ export default function Header(props) {
                     <Tab key={i} className={classes.tab} component={Link} to={route.link} label={route.name} aria-owns={route.ariaOwns} aria-haspopup={route.ariaHaspopup} onMouseOver={route.mouseOver} />
                 ))}
             </Tabs>
-            <Button variant='contained' color='secondary' className={classes.button}>Free Estimate</Button>
+            <Button variant='contained' color='secondary' className={classes.button} component={Link} to='/estimate'>Free Estimate</Button>
             <Menu id="simple-menu" anchorEl={anchorEl} keepMounted open={openMenu} onClose={handleClose} MenuListProps={{ onMouseLeave: handleClose }} classes={{ paper: classes.menu }} elevation={0}>
                 {menuOptions.map((option, i) => (
                     <MenuItem key={i} onClick={(event) => { handleMenuItemClick(event, i); setValue(1); handleClose() }} component={Link} to={option.link} className={classes.menuContainer} classes={{ root: classes.menuItem }} selected={i === selectedIndex && value === 1}>
